d7f665820bdf575d030758d2a95d62cef13e86b5
Hao Che <castleche@163.com>
2024-10-18 01:52:34 +0800

    Login. Display profile data. No time for I18n, Test or Push.

--- GitHubClient/ContentView.swift
+++ GitHubClient/ContentView.swift
@@ -6,32 +6,90 @@
 //
 
 import SwiftUI
+import Alamofire
+
+struct UserResponse: Codable {
+    let name : String?
+    let email : String?
+    let html_url : String?
+    let created_at : String?
+}
 
 struct ContentView: View {
     @State private var shouldLogin = true
+    @State public var accessToken : String = ""
     @State private var isPresentingLoginView = false
+    @State private var userData: UserResponse?
+
+    private var loginView : LoginView? = nil;
 
     var body: some View {
-        Text("Hello, world!")
-            .font(.largeTitle)
-            .foregroundColor(.blue)
-            .padding()
-            .background(.green)
-            .onAppear {
-                if (shouldLogin)
-                {
-                    isPresentingLoginView = true;
+        HStack(alignment: .center) {
+            Spacer(minLength: 30)
+            VStack(alignment: .leading) {
+                Text("Name: " + (userData?.name ?? "N/A"))
+                    .padding(20)
+                Text("Email: " + (userData?.email ?? "N/A"))
+                    .padding(20)
+                Text("HTML Page: " + (userData?.html_url ?? "N/A"))
+                    .padding(20)
+                Text("Registered On: " + (userData?.created_at ?? "N/A"))
+                    .padding(20)
+                if accessToken.count == 0 {
+                    Button(action: {
+                        if accessToken.count == 0
+                        {
+                            isPresentingLoginView = true;
+                        }
+                    }) {
+                        Text("Login")
+                            .foregroundColor(.blue)
+                            .padding(10)
+                    }
+                    .background(Color.white)
+                    .border(Color.blue, width: 2)
                 }
             }
-            .sheet(isPresented: $isPresentingLoginView)
-        {
-            LoginView()
+            Spacer(minLength: 30)
         }
+        .onAppear {
+            if accessToken.count == 0
+            {
+                isPresentingLoginView = true;
+            }
+        }
+        .sheet(isPresented: $isPresentingLoginView, onDismiss: {
+            if accessToken.count == 0 {
+                return
+            }
+            AF.request("https://api.github.com/user",
+                       method: .get,
+                       headers: ["Accept": "application/json",
+                                "Authorization": "Bearer " + accessToken,
+                                 "X-GitHub-Api-Version": "2022-11-28"])
+                .validate()
+                .responseDecodable(of: UserResponse.self) { response in
+                    switch response.result {
+                    case .success(let data):
+                        userData = data
+                    case .failure:
+                        userData = UserResponse(name: "N/A", email: "N/A", html_url: "N/A", created_at: "N/A")
+                    }
+                }
+//                .responseString { response in
+//                                 switch response.result {
+//                                 case .success(let responseString):
+//                                     print("响应字符串: \(responseString)")
+//                                 case .failure(let error):
+//                                     print( "请求失败：\(error.localizedDescription)")
+//                                 }
+//                             }
+        }, content: { LoginView(accessToken: $accessToken) })
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
+//struct ContentView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        ContentView()
+//    }
+//}
--- GitHubClient/LoginView.swift
+++ GitHubClient/LoginView.swift
@@ -7,11 +7,16 @@
 
 import Foundation
 import SwiftUI
+import Alamofire
 
 struct LoginView: View {
     @Environment(\.presentationMode) var presentationMode
 
     @State private var userInput: String = ""
+    @State private var errorHint: String = ""
+
+    @Binding public var accessToken : String
+
     var body: some View {
         VStack(alignment: .center) {
             HStack(alignment: .center) {
@@ -30,7 +35,24 @@ struct LoginView: View {
                     HStack(alignment:.center) {
                         Button(action: {
                             if userInput.count > 0 {
-                                presentationMode.wrappedValue.dismiss()
+                                AF.request("https://api.github.com/user",
+                                           method: .get,
+                                           headers: ["Accept": "application/vnd.github+json",
+                                                    "Authorization": "Bearer " + userInput,
+                                                     "X-GitHub-Api-Version": "2022-11-28"])
+                                    .validate()
+                                    .response { response in
+                                        if let statusCode = response.response?.statusCode {
+                                            if (statusCode == 200)
+                                            {
+                                                errorHint = ""
+                                                accessToken = userInput
+                                                presentationMode.wrappedValue.dismiss()
+                                            }
+                                        }
+                                    }
+                            } else {
+                                errorHint = "Please enter valid Github access token (classic) above."
                             }
                         }) {
                             Text("Login")
@@ -50,6 +72,15 @@ struct LoginView: View {
                         .border(Color.blue, width: 2)
                     }
                     Spacer().frame(height: 30)
+                    VStack()
+                    {
+                        Text(errorHint)
+                            .multilineTextAlignment(.leading)
+                            .lineLimit(10)
+                            .foregroundColor(.red)
+                            .font(.footnote)
+                    }.frame(width: 270, height: 60, alignment: .leading)
+                    Spacer().frame(height: 30)
                 }
             }
         }
@@ -57,7 +88,8 @@ struct LoginView: View {
 }
 
 //struct LoginView_Previews: PreviewProvider {
+//    @State static public var accessToken : String = ""
 //    static var previews: some View {
-//        LoginView()
+//        LoginView(accessToken: $accessToken)
 //    }
 //}
